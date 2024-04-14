e9e9638c67f85eb0ff5a467256f5796af08768d1
zhuchentong <2037630@163.com>
2024-04-14 19:56:03 +0800

    update

--- contracts/21_CallContract.sol
+++ contracts/21_CallContract.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
 contract OtherContract {
     uint256 private _x = 0;
 
@@ -6,8 +9,34 @@ contract OtherContract {
     function getBalance() view public returns(uint){
         return address(this).balance;
     }
+
+    function setX(uint256 x) external payable{
+        _x = x;
+
+        if(msg.value > 0){
+            emit Log(msg.value, gasleft());
+        }
+    }
+
+    function getX() external view returns(uint256){
+        return _x;
+    }
 }
 
 contract CallContract {
-    
+    function callSetX(address _address, uint x) external {
+        OtherContract(_address).setX(x);
+    }
+
+    function getX_1(address _address) external view returns(uint){
+        return OtherContract(_address).getX();
+    }
+
+    function getX_2(OtherContract _address)  external view returns(uint){
+        return _address.getX();
+    }
+
+    function setXTransferETH(address _address, uint x) payable external {
+        OtherContract(_address).setX{value: msg.value}(x);
+    }
 }
